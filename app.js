95adf7176cfd3d606553eb0743a63fa4b35aff99
akmalluthfi <akmalluthfi19@gmail.com>
2022-09-16 18:44:45 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,2 @@
+// ketika browser berhasil di loaded, jalankan fungsi main
+document.addEventListener('DOMContentLoaded', main);
---
+++ src/script/view/main.js
@@ -0,0 +1,51 @@
+var main = function () {
+  // cari element yang dibutuhkan
+  var searchElement = document.querySelector('#searchElement');
+  var buttonSearchElement = document.querySelector('#searchButtonElement');
+  var clubListElement = document.querySelector('#clubList');
+
+  // beri fungsi untuk event cllick button
+  var onButtonSearchClicked = function () {
+    // buat object pada data source dan berisi paramater fungsi
+    // jika berhasil => 0
+    // jika gagal => 1
+    var dataSource = new DataSource(renderResult, fallbackResult);
+    // jalankan method search club pada class data source
+    // parameter berisi value input
+    console.log(dataSource);
+    dataSource.searchClub(searchElement.value);
+  };
+
+  var renderResult = function (results) {
+    clubListElement.innerHTML = '';
+    results.forEach(function (club) {
+      var name = club.name;
+      var fanArt = club.fanArt;
+      var description = club.description;
+
+      var clubElement = document.createElement('div');
+      clubElement.setAttribute('class', 'club');
+
+      clubElement.innerHTML =
+        '<img class="fan-art-club" src="' +
+        fanArt +
+        '" alt="Fan Art">\n' +
+        '<div class="club-info">\n' +
+        '<h2>' +
+        name +
+        '</h2>\n' +
+        '<p>' +
+        description +
+        '</p>' +
+        '</div>';
+      clubListElement.appendChild(clubElement);
+    });
+  };
+
+  var fallbackResult = function (message) {
+    clubListElement.innerHTML = '';
+    clubListElement.innerHTML += '<h2 class="placeholder">' + message + '</h2>';
+  };
+
+  buttonSearchElement.addEventListener('click', onButtonSearchClicked);
+};
